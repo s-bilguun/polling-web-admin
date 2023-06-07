7e01bd015e77b57da8970c91fc00799a91e1589a
Undercroft0 <seekndestroy0501@gmail.com>
2023-06-07 09:22:22 +0800

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,108 @@
-import Image from 'next/image'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import '../../src/app/admin-page.css';
 
-export default function Home() {
+const AdminPage = () => {
+  const polls = [
+    { id: 1, question: 'What is your favorite color?' },
+    { id: 2, question: 'How often do you exercise?' },
+  ];
+
+  const router = useRouter();
+
+  // State to store the list of registered users
+  const [users, setUsers] = useState([
+    { id: 1, name: 'User 1', canCreatePoll: true },
+    { id: 2, name: 'User 2', canCreatePoll: false },
+    { id: 3, name: 'User 3', canCreatePoll: false },
+    // Add more user objects as needed
+  ]);
+
+  const handlePollClick = (id) => {
+    router.push(`/admin/polls/${id}`);
+  };
+
+  const handleRemoveUser = (userId) => {
+    // Logic to remove the user from the backend
+    // After successful removal, update the users state
+    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+  };
+
+  const handleTogglePollCreation = (userId) => {
+    // Logic to toggle the user's poll creation access in the backend
+    // After successful toggle, update the users state
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === userId ? { ...user, canCreatePoll: !user.canCreatePoll } : user
+      )
+    );
+  };
+
+  const [showUserList, setShowUserList] = useState(false);
+
+  const handleUserManagementClick = () => {
+    setShowUserList(!showUserList);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Admin Page</h1>
+
+      <h2 className="text-2xl font-semibold mb-2">Polls</h2>
+      <ul className="mb-4">
+        {polls.map((poll) => (
+          <li
+            key={poll.id}
+            className="text-blue-500 cursor-pointer"
+            onClick={() => handlePollClick(poll.id)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <Link href={`/admin/polls/${poll.id}`}>
+              <div>{poll.question}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Button for user management */}
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded manage-users-button"
+        onClick={handleUserManagementClick}
+      >
+        Manage Users
+      </button>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* List of registered users */}
+      {showUserList && (
+        <>
+          <h2 className="text-2xl font-semibold mb-2 mt-8">Registered Users</h2>
+          <ul className="mb-4">
+            {users.map((user) => (
+              <li key={user.id} className="flex items-center justify-between">
+                <div>{user.name}</div>
+                <div>
+                  <button
+                    className="text-red-500 mr-2 remove-button"
+                    onClick={() => handleRemoveUser(user.id)}
+                  >
+                    Remove
+                  </button>
+                  <button
+                    className={`${
+                      user.canCreatePoll ? 'text-green-500' : 'text-gray-500'
+                    } disable-button`}
+                    onClick={() => handleTogglePollCreation(user.id)}
+                  >
+                    {user.canCreatePoll ? 'Disable Poll Creation' : 'Enable Poll Creation'}
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default AdminPage;
---
+++ src/app/polls/[pollId].js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Bar } from 'react-chartjs-2';
+import { BarController, BarElement, LinearScale, CategoryScale, Chart } from 'chart.js';
+
+// Register the LinearScale with Chart.js
+Chart.register(BarController, BarElement, LinearScale, CategoryScale);
+
+const PollPage = () => {
+  const router = useRouter();
+  const { pollid } = router.query;
+
+  // Fetch poll details based on pollid and populate the poll object
+  const poll = {
+    id: pollid,
+    question: 'What is your favorite color?',
+    startDate: '2023-06-01',
+    expireDate: '2023-06-30',
+    options: [
+      { id: 1, text: 'Red', votes: 10 },
+      { id: 2, text: 'Blue', votes: 15 },
+      { id: 3, text: 'Green', votes: 8 },
+    ],
+    
+  };
+
+  
+
+  // Function to handle deletion of the poll
+  const handleDelete = () => {
+    // Delete the poll logic
+
+    // After deletion, navigate back to the admin page
+    router.push('/admin');
+  };
+
+  // Prepare the data for the chart
+  const chartData = {
+    labels: poll.options.map((option) => option.text),
+    datasets: [
+      {
+        label: 'Votes',
+        data: poll.options.map((option) => option.votes),
+        backgroundColor: 'rgba(75, 132, 192, 0.6)',
+      },
+    ],
+  };
+
+
+// Configure the options for the chart
+const chartOptions = {
+  type: 'bar',
+  scales: {
+    x: {
+      type: 'category',
+      labels: poll.options.map((option) => option.text),
+      ticks: {
+        color: '#2986cc ', // Set the x-axis label color based on the color scheme
+      },
+    },
+    y: {
+      beginAtZero: true,
+      ticks: {
+        stepSize: 1,
+        color: '#2986cc ', // Set the y-axis label color based on the color scheme
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      labels: {
+        color: '#2986cc ', // Set the legend label color to black
+      },
+    },
+  },
+};
+
+
+  return (
+    <div>
+      <h1>Poll Details</h1>
+      <h2>Question: {poll.question}</h2>
+      <p>Start Date: {poll.startDate}</p>
+      <p>Expire Date: {poll.expireDate}</p>
+
+      {/* Render poll options with vote counts */}
+      <ul>
+        {poll.options.map((option) => (
+          <li key={option.id}>
+            Option: {option.text}, Votes: {option.votes}
+          </li>
+        ))}
+      </ul>
+
+      {/* Chart for displaying the poll results */}
+      <div>
+        <h3>Poll Results</h3>
+        <Bar data={chartData} options={chartOptions} />
+      </div>
+
+      {/* Button for deleting the poll */}
+      <button onClick={handleDelete}>Delete Poll</button>
+    </div>
+  );
+};
+
+export default PollPage;
