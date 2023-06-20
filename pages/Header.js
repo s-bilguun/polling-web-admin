5a0a18b27e171181591c462284a0d4b69dfd08b6
temuujin <temvvjint27@gmail.com>
2023-06-20 14:28:03 +0800

    night and light mode

--- pages/Header.js
+++ pages/Header.js
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import { AuthContext } from './AuthContext';
 import Cookies from 'js-cookie';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
@@ -26,6 +28,32 @@ const Header = () => {
     }
   }, [user]);
 
+  const [darkTheme, setDarkTheme] = useState(false);
+  const handleToggle = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  useEffect(() => {
+    const storedTheme = window.localStorage.getItem('theme');
+    if (storedTheme === 'dark') {
+      setDarkTheme(true);
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      setDarkTheme(false);
+      document.documentElement.removeAttribute('data-theme');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkTheme) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      window.localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.removeAttribute('data-theme');
+      window.localStorage.setItem('theme', 'light');
+    }
+  }, [darkTheme]);
+
   return (
     <header>
       <nav>
@@ -37,6 +65,16 @@ const Header = () => {
               </div>
             </Link>
           </li>
+          <li>
+            <div className="toggle-container">
+              <FontAwesomeIcon
+                icon={darkTheme ? faMoon : faSun}
+                className={`toggle-icon ${darkTheme ? 'moon' : 'sun'}`}
+                onClick={handleToggle}
+              />
+            </div>
+          </li>
+
           <li className="header-item header-item-spacer">
             <Link href="/register_user">
               <button className="add-users-button"><FontAwesomeIcon icon={faUserPlus} /> Add User</button>
